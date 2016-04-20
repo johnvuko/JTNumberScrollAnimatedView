908f396ac9c023b07a27344763ec5b55e11ce45a
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2016-04-20 14:40:11 +0700

    Demo update

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -19,13 +19,14 @@
     
     self.animatedView.textColor = [UIColor whiteColor];
     self.animatedView.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:42];
-    
-    self.animatedView.minLength = 3;
+    self.animatedView.duration = 5.0;
+    self.animatedView.durationOffset = 1.0;
+    self.animatedView.minLength = 4;
 }
 
 - (IBAction)didStartAnimationTouch:(id)sender
 {
-    [self.animatedView setValue:[NSNumber numberWithInt:(rand() % 5000)]];
+    [self.animatedView setValue:0];
     [self.animatedView startAnimation];
 }
 
